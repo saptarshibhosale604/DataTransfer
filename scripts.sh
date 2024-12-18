e283051e686b2b5a30b44815133b3dda2b7a329a
Saptarshi Bhosale <61815159+saptarshibhosale604@users.noreply.github.com>
2024-12-18 16:33:10 +0530

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,23 @@
+####  loggin.py ####  
+import logging
+
+def main() -> None:
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename="Logs/basic.log",
+    )
+
+    logging.debug("This is a debug message.")
+    logging.info("This is an info message.")
+    logging.warning("This is a warning message.")
+    logging.error("This is an error message.")
+    logging.critical("This is a critical message.")
+
+if __name__ == "__main__":
+    main()
+
 ####  timer.sh ####  
 # e.g 
 # timer.sh 1 sec
